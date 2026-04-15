40ccfd20e59adc7e6292b99624d0b23da73435bb
Aarongel205 <aaronadanza92@gmail.com>
2026-04-15 23:31:33 +0800

    added time & datasets

--- Sequential vs Parallel Algorithms/Sequential & Parallel Sorting/ParallelMergeSort.py
+++ Sequential vs Parallel Algorithms/Sequential & Parallel Sorting/ParallelMergeSort.py
@@ -35,3 +35,27 @@ def parallelSort(nums):
                 k += 1
     return nums
 
+small = [random.randint(1, 1000) for _ in range(10)]
+#medium = [random.randint(1, 100000) for _ in range(10)]
+#large = [random.randint(1, 1000000) for _ in range(10)]
+
+reverseSorted = sorted(small, reverse=True)
+
+chunk_size = len(reverseSorted) // 4
+
+chunks = [reverseSorted[i:i + chunk_size] for i in range(0, len(reverseSorted),
+chunk_size)]
+
+start = time.perf_counter()
+
+with Pool(4) as p:
+    sorted_chunks = p.map(parallelSort, chunks)
+
+mergedChunks = list(heapq.merge(*sorted_chunks))
+
+print(mergedChunks)
+
+end = time.perf_counter()
+
+print(f'total time: {end - start:.6f}')
+
