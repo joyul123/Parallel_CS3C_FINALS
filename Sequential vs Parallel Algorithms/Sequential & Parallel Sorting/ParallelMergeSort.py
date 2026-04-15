8df1f795342a7e68633e67c4763f24388ded481c
Aarongel205 <aaronadanza92@gmail.com>
2026-04-15 23:29:45 +0800

    added parallelMS function

--- Sequential vs Parallel Algorithms/Sequential & Parallel Sorting/ParallelMergeSort.py
+++ Sequential vs Parallel Algorithms/Sequential & Parallel Sorting/ParallelMergeSort.py
@@ -3,3 +3,35 @@ import time
 from multiprocessing import Pool
 import heapq
 
+#merge sort
+def parallelSort(nums):
+    if len(nums) > 1:
+        left = nums[:len(nums)//2]
+        right = nums[len(nums)//2:]
+
+        #recursion
+        parallelSort(left)
+        parallelSort(right)
+
+        #merge
+        i = 0 
+        j = 0 
+        k = 0 
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                nums[k] = left[i]
+                i += 1
+            else:
+                nums[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+                nums[k] = left[i]
+                i += 1
+                k += 1
+        while j < len(right):
+                nums[k] = right[j]
+                j += 1
+                k += 1
+    return nums
+
